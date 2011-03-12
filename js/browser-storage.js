16649885abd8147748933d27481460cd43b2739b
Alistair Jones <alistair.jones@gmail.com>
2011-03-12 22:18:02 +0000

    Store both style and content

--- js/browser-storage.js
+++ js/browser-storage.js
@@ -3,13 +3,6 @@ var browserStorage = function() {
 
 	var diagramStorageKey = null;
 	
-	browserStoragePackage.loadLastOpenDocumentIfExists = function() {
-		var lastOpenDocument = localStorage.getItem("boxesAndArrows.lastOpenDocument");
-		if (lastOpenDocument) {
-			$("#ba-content-editor").val(localStorage.getItem("boxesAndArrows.source." + lastOpenDocument));
-		}
-	}
-
 	var refeshExistingDocumentsList = function() {
 		$(".ba-existing-documents-list").find("*").remove();
 		for (var i = 0; i < localStorage.length; i++) {
@@ -17,30 +10,6 @@ var browserStorage = function() {
 			if (key.indexOf("boxesAndArrows.source.") === 0) {
 				$(".ba-existing-documents-list").append("<option>" + key.substr("boxesAndArrows.source.".length) + "</option>");
 			}
-		}
-	}
-
-	var open = function() {
-		diagramStorageKey = $("#ba-open-storage-key").val();
-		$("#ba-content-editor").val(localStorage.getItem("boxesAndArrows.source." + diagramStorageKey));
-		localStorage.setItem("boxesAndArrows.lastOpenDocument", diagramStorageKey);
-		window.history.back();
-		editorChanged();
-	}
-
-	var saveAs = function() {
-		diagramStorageKey = $("#ba-save-as-storage-key").val();
-		save();
-		window.history.back();
-	}
-
-	var save = function() {
-		if (diagramStorageKey) {
-			localStorage.setItem("boxesAndArrows.source." + diagramStorageKey, $("#ba-content-editor").val());
-			localStorage.setItem("boxesAndArrows.lastOpenDocument", diagramStorageKey);
-			showSaveSuccessfulMessage();
-		} else {
-			window.location.hash = "save-as";
 		}
 	}
 
@@ -54,24 +23,60 @@ var browserStorage = function() {
 		})
 	}
 
-	browserStoragePackage.bindStorageButtons = function() {
-		$(".ba-toggle-editor").click(function() {
-			$(".ba-editor").toggleClass("hidden");
-		});
-		$(".ba-button-open").click(function() {
-			refeshExistingDocumentsList();
-			window.location.hash = "open";
-		});
-		$(".ba-button-save").click(save);
-		$(".ba-button-save-as").click(function() {
-			refeshExistingDocumentsList();
-			window.location.hash = "save-as";
-		});
-		$("#ba-button-open-ok").click(open);
-		$("#ba-button-save-as-ok").click(saveAs);
-		$(".ba-existing-documents-list").change(function() {
-			$(".ba-diagram-storage-key").val($(".ba-existing-documents-list").val());
-		});
+	browserStoragePackage.init = function(getCurrentState, setCurrentState) {
+		var open = function() {
+			diagramStorageKey = $("#ba-open-storage-key").val();
+			setCurrentState(localStorage.getItem("boxesAndArrows.source." + diagramStorageKey));
+			localStorage.setItem("boxesAndArrows.lastOpenDocument", diagramStorageKey);
+			window.history.back();
+			editorChanged();
+		}
+
+		var saveAs = function() {
+			diagramStorageKey = $("#ba-save-as-storage-key").val();
+			save();
+			window.history.back();
+		}
+
+		var save = function() {
+			if (diagramStorageKey) {
+				localStorage.setItem("boxesAndArrows.source." + diagramStorageKey, getCurrentState());
+				localStorage.setItem("boxesAndArrows.lastOpenDocument", diagramStorageKey);
+				showSaveSuccessfulMessage();
+			} else {
+				window.location.hash = "save-as";
+			}
+		}
+
+		var loadLastOpenDocumentIfExists = function() {
+			var lastOpenDocument = localStorage.getItem("boxesAndArrows.lastOpenDocument");
+			if (lastOpenDocument) {
+				setCurrentState(localStorage.getItem("boxesAndArrows.source." + lastOpenDocument));
+			}
+		}
+
+		var bindStorageButtons = function() {
+			$(".ba-toggle-editor").click(function() {
+				$(".ba-editor").toggleClass("hidden");
+			});
+			$(".ba-button-open").click(function() {
+				refeshExistingDocumentsList();
+				window.location.hash = "open";
+			});
+			$(".ba-button-save").click(save);
+			$(".ba-button-save-as").click(function() {
+				refeshExistingDocumentsList();
+				window.location.hash = "save-as";
+			});
+			$("#ba-button-open-ok").click(open);
+			$("#ba-button-save-as-ok").click(saveAs);
+			$(".ba-existing-documents-list").change(function() {
+				$(".ba-diagram-storage-key").val($(".ba-existing-documents-list").val());
+			});
+		}
+
+		bindStorageButtons();
+		loadLastOpenDocumentIfExists();
 	}
 	
 	return browserStoragePackage;
